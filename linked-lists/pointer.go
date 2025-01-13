4054d3e041483dcca40f4fa03ae80ed63d8eeeff
halladj <halladj00@gmail.com>
2025-01-13 22:40:51 +0100

    added pointer implementation

--- linked-lists/pointer.go
+++ linked-lists/pointer.go
@@ -1,1 +1,28 @@
 package linkedlists
+
+type LinkedL struct {
+	info interface{}
+	suiv *LinkedL
+}
+
+// Access implements LinkedList.
+func (l *LinkedL) Access(position int) (interface{}, error) {
+	panic("unimplemented")
+}
+
+// Add implements LinkedList.
+func (l *LinkedL) Add(position int, element interface{}) error {
+	panic("unimplemented")
+}
+
+// Delete implements LinkedList.
+func (l *LinkedL) Delete(element interface{}) error {
+	panic("unimplemented")
+}
+
+// EmptyList implements LinkedList.
+func (l *LinkedL) EmptyList() bool {
+	panic("unimplemented")
+}
+
+var _ LinkedList = (*LinkedL)(nil)
